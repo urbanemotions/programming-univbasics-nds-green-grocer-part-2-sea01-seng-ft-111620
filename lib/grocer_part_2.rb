c09595db2b8fa6862bcdeedaba8664763a99cdd2
Sin Wildy <sw.wildy@gmail.com>
2020-11-16 04:12:20 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -6,7 +6,7 @@ def apply_coupons(cart, coupons)
   # REMEMBER: This method **should** update cart
   counter = 0 
   while counter < coupons.length 
-    cart_item = find_item_by_name_in_collection(name, collection)
+    cart_item = find_item_by_name_in_collection(coupons[:item], cart)
   
 end
 
